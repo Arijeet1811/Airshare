312db27dae4fa285d5327772dd52c5de5219e781
Arijeet1811 <ashokmanna14101975@gmail.com>
2026-05-04 21:22:23 +0530

    refactor: Update Gradle dependencies and plugins
    Refactor Gradle configuration to use updated plugin aliases and library naming conventions for better maintainability. This change also includes updating the Gradle wrapper version in the CI workflow to ensure consistent build environments.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    id("com.google.firebase.appdistribution") version "5.1.1" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.composeCompiler) apply false
 }
